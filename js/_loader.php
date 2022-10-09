d45670031a06d8b36518907da2f5f4c4f1bf6d1f
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2022-10-10 03:53:45 +1100

    Remove all remaining legacy PHP build system

--- js/_loader.php
+++
@@ -1,9 +0,0 @@
-"use strict";
-<?php
-require "globals.js";
-require "cell.js";
-require "helper.js";
-require "canvas.js";
-require "ui.js";
-require "io.js";
-require "setup.js";
--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-import "jquery-ui/dist/themes/base/jquery-ui.min.css"
 import "popper.js/dist/popper.min"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min"
 
-import "bootstrap-toggle/css/bootstrap2-toggle.min.css"
 import "bootstrap-switch/dist/js/bootstrap-switch.min"
 import "bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css"
 
-
+import "jquery-ui/dist/themes/base/jquery-ui.min.css"
 import "font-awesome/css/font-awesome.min.css"
 
 import { IconColorChange, ShowLoadWarning } from "./ui";
